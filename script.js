2841ce8c1e6d43345c2aa55512003a4301d50f68
Cherch <b.cherchiglia@gmail.com>
2023-05-09 17:14:38 -0400

    Complete and Ready to Play

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const winningCombos = [
 ];
 
 /*----- app's state (variables) -----*/
-let board, turn, winner;
+let board, turn, winner
 
 /*----- cached element references -----*/
 const message = document.querySelector('h2');
